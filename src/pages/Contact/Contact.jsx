6694e3ad4af597ce1cf155de658e68d4708f1093
sadig05 <Sadiq.letifli70@gmail.com>
2022-12-24 23:44:41 +0400

    first commit

---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './Contact.module.css'
+
+const Contact = () => {
+  return (
+    <div className={style["contact"]} >Contact</div>
+  )
+}
+
+export default Contact
