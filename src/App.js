dd3dca8e9b5971602f60cd5f36a3d7907f65997e
sadig05 <Sadiq.letifli70@gmail.com>
2022-12-25 23:40:56 +0400

    router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import {Route, Routes } from "react-router-dom";
 import Contact from "./pages/Contact/Contact"
-
-
+import Movies from "./pages/Movies/Movies"
+import Home from './pages/Home/Home'
 function App() {
   return (
-    <>
-      <Contact/>
-    </>
+   <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/mvoies" element={<Movies/>}/>
+    <Route path="/contact" element={<Contact/>}/>
+   </Routes>
   );
 }
 
