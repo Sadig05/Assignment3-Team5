e6f1a612355f5ab867c4c0a85d7e8354a775eec1
NijatShv <78231857+NijatShv@users.noreply.github.com>
2023-01-04 14:44:19 +0400

    Update Movie.jsx

--- src/pages/Movie/Movie.jsx
+++ src/pages/Movie/Movie.jsx
@@ -1,26 +1,25 @@
-import React, { useState , useEffect} from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useState , useEffect} from 'react';
+import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import styles from './Movie.module.css';
 
-
 const Movie = () => {
-  const {id} = useParams();
+  const { id } = useParams();
   const [data, setData] = useState({});
   const getData = async () => {
-    const { data } = await axios.get(`http://localhost:3004/movies`);
-    const movie = data.find((item) => item.id == id);
-    
-    //getting poster url from public api
+    const { data: movies } = await axios.get(`http://localhost:3004/movies`);
+    const movie = movies.find(item => item.id == id);
+
+    // Getting poster URL from public API
     const poster = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=0a1c8756239bce4542080405c0a77a97&query=${movie.Series_Title}`);
     movie.Poster_Link = poster.data.results[0].poster_path;
     setData(movie);
   };
 
   useEffect(() => {
-    getData() 
-  },[])
+    getData();
+  }, []);
 
   return (
     <section className={styles.container}>
@@ -30,40 +29,44 @@ const Movie = () => {
       <div className={styles.details}>
         <h2 className={styles.title}>{data.Series_Title}</h2>
         <div className={styles.released}>
-          <span>Released:</span> {data.Released_Year}
+          <span className={styles.label}>Released:</span> {data.Released_Year}
         </div>
         <div className={styles.certificate}>
-          <span>Certificate:</span> {data.Certificate}
+          <span className={styles.label}>Certificate:</span> {data.Certificate}
         </div>
         <div className={styles.duration}>
-          <span>Duration:</span> {data.Runtime}
+          <span className={styles.label}>Duration:</span> {data.Runtime}
         </div>
         <div className={styles.genre}>
-          <span>Genre:</span> {data.Genre}
+          <span className={styles.label}>Genre:</span> {data.Genre}
         </div>
         <div className={styles.rating}>
-          <span>IMDB Rating:</span> {data.IMDB_Rating}
+          <span className={styles.label}>IMDB Rating:</span> {data.IMDB_Rating}
         </div>
-        <p className={styles.overview}>{data.Overview}</p>
+        <div className={styles.overview}>
+          <span className={styles.label}>Overview: </span>
+        {data.Overview}
+        </div>
         <div className={styles.meta}>
-          <span>Meta Score:</span> {data.Meta_score}
+          <span className={styles.label}>Meta Score:</span> {data.Meta_score}
         </div>
         <div className={styles.director}>
-          <span>Director:</span> {data.Director}
+          <span className={styles.label}>Director:</span> {data.Director}
         </div>
         <div className={styles.cast}>
-          <span>Starring:</span> {data.Star1}, {data.Star2}, {data.Star3}, {data.Star4}
+          <span className={styles.label}>Starring:</span> {data.Star1}, {data.Star2}, {data.Star3}, {data.Star4}
         </div>
         <div className={styles.votes}>
-          <span>Votes:</span> {data.No_of_Votes}
+          <span className={styles.label}>Votes:</span> {data.No_of_Votes}
         </div>
         <div className={styles.gross}>
-          <span>Gross:</span> {data.Gross}
+          <span className={styles.label}>Gross:</span> {data.Gross}
         </div>
         <Link to="/movies" className={styles.back}>Back to Movies List</Link>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Movie
+export default Movie;
+
